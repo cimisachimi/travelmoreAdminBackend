4fe432ea5d1e58e3d9529e4c6b7915ed78b4bb68
cimisachimi <mfaturrahmann@gmail.com>
2025-12-24 17:14:34 +0700

    fix image url

--- resources/views/vendor/notifications/header.blade.php
+++ resources/views/vendor/notifications/header.blade.php
@@ -1,6 +1,7 @@
 <tr>
     <td class="header" align="center" style="padding: 25px 0; text-align: center;">
-        <a href="{{ $url }}" style="display: inline-block;">
+        {{-- Use config('app.url') because $url is not defined in this scope --}}
+        <a href="{{ config('app.url') }}" style="display: inline-block;">
             <img src="{{ asset('images/logo.png') }}"
                  style="height: 60px; width: auto; margin: 0 auto; display: block;"
                  alt="Travelmore Logo">
