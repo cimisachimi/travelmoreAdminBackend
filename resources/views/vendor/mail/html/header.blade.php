8c22ebe3f76e2b90cb4edafea22ef4c04d74dddb
cimisachimi <mfaturrahmann@gmail.com>
2025-12-25 10:37:35 +0700

    fix header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,14 +2,15 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (file_exists(public_path('images/logo.png')))
-    <img src="{{ $message->embed(public_path('images/logo.png')) }}"
-         width="180"
-         alt="{{ config('app.name') }}"
-         style="display: block; border: 0;">
-@else
-    {{ config('app.name') }}
-@endif
+    {{-- Using a direct URL instead of $message->embed avoids attachments entirely --}}
+    @if (file_exists(public_path('images/logo.png')))
+        <img src="{{ config('app.url') }}/images/logo.png"
+             width="180"
+             alt="{{ config('app.name') }}"
+             style="display: block; border: 0;">
+    @else
+        {{ config('app.name') }}
+    @endif
 </a>
 </td>
 </tr>
